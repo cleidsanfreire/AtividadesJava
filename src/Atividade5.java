a0d4f6f21951b1aaa5d0fa07d0b9e2b97d9e1e06
Cleidsan Freire <128002022+cleidsanfreire@users.noreply.github.com>
2024-08-26 17:11:00 -0300

    Add files via upload

---
+++ src/Atividade5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Atividade5 {
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+        double nota1 = tec.nextDouble();
+
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = tec.nextDouble();
+
+        System.out.println("Digite a terceira nota: ");
+        double nota3 = tec.nextDouble();
+
+        double nota = nota1 + nota2 + nota3;
+
+        double media = nota / 3;
+        System.out.printf("A Media é: %.2f", media);
+    }
+}
