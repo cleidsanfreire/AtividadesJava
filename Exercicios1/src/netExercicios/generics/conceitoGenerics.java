c66ee1be84d55562b56c6d31079239c00e035aa2
cleidsanfreire <dsanmike12@gmail.com>
2024-09-12 16:25:32 -0300

    Re-Organizando

--- Exercicios1/src/netExercicios/generics/conceitoGenerics.java
+++ Exercicios1/src/netExercicios/generics/conceitoGenerics.java
@@ -2,20 +2,33 @@ package netExercicios.generics;
 
 public class conceitoGenerics {
     public static void main(String[] args) {
+        // Fixando que o generics que eu quero seja com numeros
+        System.out.println("Com extends");
+
         Caixa<Integer> caixaInteiro = new Caixa<>();
         caixaInteiro.adicionarConteudo(123);
 
-        System.out.println("Adicionando o numero: " + caixaInteiro);
+        System.out.println("Adicionando o numero do T(type) Integer: " + caixaInteiro);
 
         Caixa<Double> caixaDouble = new Caixa<>();
 
         caixaDouble.adicionarConteudo(152.65);
 
-        System.out.println(caixaDouble);
+        System.out.println("Adicionando o numero do T(type) Double: " + caixaDouble);
 
-        CaixaSemExtends<String> caixaString = new CaixaSemExtends<>();
-        caixaString.adcionar("Olá, Mundo!");
-        System.out.println(caixaString);
+        // Trabalhando com liberdade com o generics
+        System.out.println("\nSem extends");
+        CaixaSemExtends<String> caixaSemExtendsString = new CaixaSemExtends<>();
+        caixaSemExtendsString.adcionar("Olá, Mundo!");
+        System.out.println("Adicionando a String do T(type) String: " + caixaSemExtendsString);
+
+        CaixaSemExtends<Integer> caixaSemExtendsInteger = new CaixaSemExtends<>();
+        caixaSemExtendsInteger.adcionar(123);
+        System.out.println("Adicionando o numero do T(type) Integer: " + caixaSemExtendsInteger);
+
+        CaixaSemExtends<Double> caixaSemExtendsDouble = new CaixaSemExtends<>();
+        caixaSemExtendsDouble.adcionar(123.652);
+        System.out.println("Adicionando o numero do T(type) Double: " + caixaSemExtendsDouble);
 
     }
 }
